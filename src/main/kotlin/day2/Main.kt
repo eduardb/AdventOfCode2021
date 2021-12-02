e5087cd42037e118bef651642d25c18ec89005cc
Eduard-Cristian Bolos <eduardcbolos@gmail.com>
2021-12-02 18:10:02 +0100

    Day 2 solution

---
+++ src/main/kotlin/day2/Main.kt
@@ -0,0 +1,73 @@
+package day2
+
+import java.io.File
+
+fun main() {
+    val commands = File("src/main/kotlin/day2/input.txt").readLines().map { Command.parseCommand(it) }
+
+    val submarinePartOne = Submarine()
+    commands.forEach { it.visitPartOne(submarinePartOne) }
+    println(submarinePartOne.run { depth * position })
+
+    val submarinePartTwo = Submarine()
+    commands.forEach { it.visitPartTwo(submarinePartTwo) }
+    println(submarinePartTwo.run { depth * position })
+
+}
+
+class Submarine {
+    var depth = 0
+    var position = 0
+    var aim = 0
+}
+
+sealed class Command(open val value: Int) {
+
+    companion object {
+        fun parseCommand(input: String): Command {
+            val (rawCommand, rawValue) = input.split(" ")
+            val value = rawValue.toInt()
+            return when (rawCommand) {
+                Forward::class.simpleName!!.lowercase() -> Forward(value)
+                Down::class.simpleName!!.lowercase() -> Down(value)
+                Up::class.simpleName!!.lowercase() -> Up(value)
+                else -> throw IllegalArgumentException("$rawCommand is not a valid command")
+            }
+        }
+    }
+
+    abstract fun visitPartOne(submarine: Submarine)
+    abstract fun visitPartTwo(submarine: Submarine)
+
+    class Forward(value: Int) : Command(value) {
+        override fun visitPartOne(submarine: Submarine) {
+            submarine.position += value
+        }
+
+        override fun visitPartTwo(submarine: Submarine) {
+            submarine.position += value
+            submarine.depth += submarine.aim * value
+        }
+    }
+
+    class Down(value: Int) : Command(value) {
+        override fun visitPartOne(submarine: Submarine) {
+            submarine.depth += value
+        }
+
+        override fun visitPartTwo(submarine: Submarine) {
+            submarine.aim += value
+        }
+
+    }
+
+    class Up(value: Int) : Command(value) {
+        override fun visitPartOne(submarine: Submarine) {
+            submarine.depth -= value
+        }
+
+        override fun visitPartTwo(submarine: Submarine) {
+            submarine.aim -= value
+        }
+    }
+}
