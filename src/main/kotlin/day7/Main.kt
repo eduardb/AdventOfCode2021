43fc58c83d042902ca9b56e55fd4a49f5144a94f
Eduard-Cristian Bolos <eduardcbolos@gmail.com>
2021-12-07 10:02:40 +0100

    Day 7 solution

---
+++ src/main/kotlin/day7/Main.kt
@@ -0,0 +1,25 @@
+package day7
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val input = File("src/main/kotlin/day7/input.txt").readText().split(",").map(String::toInt)
+
+    val min = input.minOrNull() ?: 0
+    val max = input.maxOrNull() ?: 0
+
+    val minFuel1 = (min..max).minOfOrNull { position ->
+        input.sumOf { abs(it - position) }
+    }
+
+    println(minFuel1)
+
+    val minFuel2 = (min..max).minOfOrNull { position ->
+        input.sumOf { sumFrom1ToN(abs(it - position)) }
+    }
+
+    println(minFuel2)
+}
+
+fun sumFrom1ToN(n: Int) = (1 + n) * n / 2
